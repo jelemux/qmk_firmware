266c3286022fc95d9b6cbdc7833470aa87449de3
jelemux <jeremias.weber@protonmail.com>
2023-06-21 19:35:12 +0200

    Optimize layers

--- keyboards/hillside/52/keymaps/jelemux/keymap.c
+++ keyboards/hillside/52/keymaps/jelemux/keymap.c
@@ -21,7 +21,7 @@ enum layers {
 
 #define LY_BASE TO(_BASE)
 #define LY_MED OSL(_MEDIA)
-#define LY_GAM OSL(_GAMING)
+#define LY_GAM TO(_GAMING)
 #define LY_ADJ OSL(_ADJUST)
 #define ALT_GR OSM(MOD_RALT)
 #define OSM_LSFT OSM(MOD_LSFT)
@@ -43,25 +43,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
      OSM_LCT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,                       KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM_RCT,
-     LY_MED,  KC_LGUI, KC_LALT,          ALT_GR,  KC_CAPS, KC_SPC, OSM_LSFT,   OSM_RSFT, KC_SPC , KC_CAPS, ALT_GR,           KC_MINS, LY_GAM,  LY_ADJ
+     KC_LALT, KC_LGUI, LY_MED,           ALT_GR,  KC_CAPS, KC_SPC, OSM_LSFT,   OSM_RSFT, KC_SPC , KC_CAPS, ALT_GR,           KC_MINS, KC_EQUAL,KC_GRAVE
     ),
     [_MEDIA] = LAYOUT(
      KC_VOLU, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_HOME,
      KC_VOLD, KC_F11 , KC_F12 , KC_MPLY, KC_MSTP, KC_MPRV,                                        KC_MNXT, KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_END,
      _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , UNDO   ,                      REDO   , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
-     _______, LY_BASE, _______,          KC_MUTE, CUT    , COPY   , _______,    _______, PASTE  , KC_PGUP, KC_PGDN,          XXXXXXX, _______, _______
+     _______,  LY_GAM, LY_ADJ,           KC_MUTE, CUT    , COPY   , _______,    _______, PASTE  , KC_PGUP, KC_PGDN,          XXXXXXX, _______, LY_BASE
     ),
     [_GAMING] = LAYOUT(
      KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_3,                         KC_4,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
-     KC_LCTL, LY_BASE, KC_ESC,           KC_LGUI, KC_LALT, KC_SPC,  KC_1,         KC_2,  KC_SPC,  KC_RALT, KC_RGUI,          KC_LEFT, KC_DOWN, KC_RGHT
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_4,                         KC_2,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT,
+     KC_LCTL, KC_ESC,  LY_BASE,          KC_LGUI, KC_LALT, KC_SPC,  KC_3,         KC_1,  KC_SPC,  KC_RALT, KC_RGUI,          KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_ADJUST] = LAYOUT(
      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, AG_SWAP, CG_SWAP,                                        XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
      XXXXXXX, XXXXXXX, KC_BRIU, XXXXXXX, AG_NORM, CG_NORM,                                        RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, XXXXXXX, XXXXXXX,
      QK_BOOT, XXXXXXX, KC_BRID, XXXXXXX, XXXXXXX, XXXXXXX, EE_CLR,                      RGB_TOG, RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, XXXXXXX, XXXXXXX,
-     XXXXXXX, LY_BASE, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, _______,    _______, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX
+     XXXXXXX, XXXXXXX, LY_BASE,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, LY_BASE
     ),
 };
 
