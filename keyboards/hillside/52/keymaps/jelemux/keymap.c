ba2ac07f8ddb706daa202e7d1a78bd81fa5bb7af
jelemux <jeremias.weber@protonmail.com>
2023-06-21 19:52:38 +0200

    Add other necessary keys to base layer

--- keyboards/hillside/52/keymaps/jelemux/keymap.c
+++ keyboards/hillside/52/keymaps/jelemux/keymap.c
@@ -43,13 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
      OSM_LCT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,                       KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM_RCT,
-     KC_LALT, KC_LGUI, LY_MED,           ALT_GR,  KC_CAPS, KC_SPC, OSM_LSFT,   OSM_RSFT, KC_SPC , KC_CAPS, ALT_GR,           KC_MINS, KC_EQUAL,KC_GRAVE
+     KC_LALT, KC_LGUI, LY_MED,           KC_LBRC, KC_GRV,  KC_SPC, OSM_LSFT,   OSM_RSFT, KC_SPC , ALT_GR,  KC_RBRC,          KC_MINS, KC_EQL,  KC_BSLS
     ),
     [_MEDIA] = LAYOUT(
      KC_VOLU, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_HOME,
      KC_VOLD, KC_F11 , KC_F12 , KC_MPLY, KC_MSTP, KC_MPRV,                                        KC_MNXT, KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_END,
      _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , UNDO   ,                      REDO   , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
-     _______,  LY_GAM, LY_ADJ,           KC_MUTE, CUT    , COPY   , _______,    _______, PASTE  , KC_PGUP, KC_PGDN,          XXXXXXX, _______, LY_BASE
+     _______, LY_GAM,  LY_ADJ,           KC_MUTE, CUT    , COPY   , _______,    _______, PASTE  , KC_PGUP, KC_PGDN,          _______, _______, LY_BASE
     ),
     [_GAMING] = LAYOUT(
      KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
