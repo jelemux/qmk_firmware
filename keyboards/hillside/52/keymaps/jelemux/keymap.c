7d991f0fafa858906749de14e26898a283583ead
jelemux <jeremias.weber@protonmail.com>
2023-06-23 17:52:19 +0200

    Optimize enter key placement

--- keyboards/hillside/52/keymaps/jelemux/keymap.c
+++ keyboards/hillside/52/keymaps/jelemux/keymap.c
@@ -40,10 +40,10 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
+     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,
      KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
      OSM_LCT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,                       KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OSM_RCT,
-     KC_LALT, KC_LGUI, LY_MED,           KC_LBRC, KC_GRV,  KC_SPC, OSM_LSFT,   OSM_RSFT, KC_SPC , ALT_GR,  KC_RBRC,          KC_MINS, KC_EQL,  KC_BSLS
+     KC_LALT, KC_LGUI, LY_MED,           KC_LBRC, KC_ENT,  KC_SPC, OSM_LSFT,   OSM_RSFT, KC_SPC , ALT_GR,  KC_RBRC,          KC_MINS, KC_EQL,  KC_BSLS
     ),
     [_MEDIA] = LAYOUT(
      KC_VOLU, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                                        KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_HOME,
